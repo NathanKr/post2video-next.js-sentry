ddbcb561875b66343772616c507117b14ece4624
nathankr <natankrasney@gmail.com>
2026-01-08 20:22:17 +0200

    sentry css module

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -3,6 +3,7 @@
 import * as Sentry from "@sentry/nextjs";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import styles from "../../styles/sentry.module.css";
 
 class SentryExampleFrontendError extends Error {
   constructor(message: string | undefined) {
@@ -31,8 +32,8 @@ export default function Page() {
         <meta name="description" content="Test Sentry for your Next.js app!" />
       </Head>
 
-      <main>
-        <div className="flex-spacer" />
+      <main className={styles.main}>
+        <div className={styles.flexSpacer} />
         <svg
           height="40"
           width="40"
@@ -48,7 +49,7 @@ export default function Page() {
         </svg>
         <h1>sentry-example-page</h1>
 
-        <p className="description">
+        <p className={styles.description}>
           Click the button below, and view the sample error on the Sentry{" "}
           <a
             target="_blank"
@@ -94,9 +95,9 @@ export default function Page() {
         </button>
 
         {hasSentError ? (
-          <p className="success">Error sent to Sentry.</p>
+          <p className={styles.success}>Error sent to Sentry.</p>
         ) : !isConnected ? (
-          <div className="connectivity-error">
+          <div className={styles.connectivityError}>
             <p>
               It looks like network requests to Sentry are being blocked, which
               will prevent errors from being captured. Try disabling your
@@ -104,134 +105,11 @@ export default function Page() {
             </p>
           </div>
         ) : (
-          <div className="success_placeholder" />
+          <div className={styles.successPlaceholder} />
         )}
 
-        <div className="flex-spacer" />
+        <div className={styles.flexSpacer} />
       </main>
-
-      <style>{`
-        main {
-          display: flex;
-          min-height: 100vh;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          gap: 16px;
-          padding: 16px;
-          font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif;
-        }
-
-        h1 {
-          padding: 0px 4px;
-          border-radius: 4px;
-          background-color: rgba(24, 20, 35, 0.03);
-          font-family: monospace;
-          font-size: 20px;
-          line-height: 1.2;
-        }
-
-        p {
-          margin: 0;
-          font-size: 20px;
-        }
-
-        a {
-          color: #6341F0;
-          text-decoration: underline;
-          cursor: pointer;
-
-          @media (prefers-color-scheme: dark) {
-            color: #B3A1FF;
-          }
-        }
-
-        button {
-          border-radius: 8px;
-          color: white;
-          cursor: pointer;
-          background-color: #553DB8;
-          border: none;
-          padding: 0;
-          margin-top: 4px;
-
-          & > span {
-            display: inline-block;
-            padding: 12px 16px;
-            border-radius: inherit;
-            font-size: 20px;
-            font-weight: bold;
-            line-height: 1;
-            background-color: #7553FF;
-            border: 1px solid #553DB8;
-            transform: translateY(-4px);
-          }
-
-          &:hover > span {
-            transform: translateY(-8px);
-          }
-
-          &:active > span {
-            transform: translateY(0);
-          }
-
-          &:disabled {
-	            cursor: not-allowed;
-	            opacity: 0.6;
-
-	            & > span {
-	              transform: translateY(0);
-	              border: none
-	            }
-	          }
-        }
-
-        .description {
-          text-align: center;
-          color: #6E6C75;
-          max-width: 500px;
-          line-height: 1.5;
-          font-size: 20px;
-
-          @media (prefers-color-scheme: dark) {
-            color: #A49FB5;
-          }
-        }
-
-        .flex-spacer {
-          flex: 1;
-        }
-
-        .success {
-          padding: 12px 16px;
-          border-radius: 8px;
-          font-size: 20px;
-          line-height: 1;
-          background-color: #00F261;
-          border: 1px solid #00BF4D;
-          color: #181423;
-        }
-
-        .success_placeholder {
-          height: 46px;
-        }
-
-        .connectivity-error {
-          padding: 12px 16px;
-          background-color: #E50045;
-          border-radius: 8px;
-          width: 500px;
-          color: #FFFFFF;
-          border: 1px solid #A80033;
-          text-align: center;
-          margin: 0;
-        }
-
-        .connectivity-error a {
-          color: #FFFFFF;
-          text-decoration: underline;
-        }
-      `}</style>
     </div>
   );
 }
